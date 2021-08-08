773d1bda650ba60a656e6b94b350f0b880e1abe2
Jose <J_Lanza.13@live.com>
2021-08-08 00:47:15 -0400

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,8 @@
+const h2 = document.createElement('h2');
+h2.textContent = "This content added by JavaScript";
+document.querySelector('body').appendChild(h2);
+
+const p = document.createElement('p');
+p.textContent = "Now SUBMIT or PARISH!";
+document.querySelector('body').appendChild(p);
+
